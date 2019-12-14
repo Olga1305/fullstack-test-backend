695c7b0245781358fa37571ca6dc7e93f51665b2
Olga Dokukova <dokukova@gmail.com>
2019-12-14 15:59:35 +0100

    [FET-003] close #3 pagination

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const Beer = require('../models/Beer');
 const router = express.Router();
 
-// GET 1st page beers
-router.get('/', async (req, res, next) => {
+const pageSize = 20;
+
+// GET paginated beers
+router.get('/page/:currentPage', async (req, res, next) => {
+  const { currentPage } = req.params;
+  const skip = (currentPage * pageSize) - pageSize;
   try {
-    const beers = await Beer.find().limit(24);
+    const beers = await Beer.find().limit(pageSize).skip(skip);
     res.json(beers);
   } catch (error) {
     next(error);
